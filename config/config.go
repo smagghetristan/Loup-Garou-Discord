6592f6c7276528be8747f62748ced8532e189cc3
smagghetristan <31407909+smagghetristan@users.noreply.github.com>
2019-04-16 23:42:49 +0200

    Moved secret token

---
+++ config/config.go
@@ -0,0 +1,72 @@
+package config
+
+import (
+	"github.com/bwmarrin/discordgo"
+	"github.com/gorilla/websocket"
+)
+
+var ImagePath = "static/"
+
+//IDs
+var VoiceChannel = "398084144862724098"
+var GuildID = "248880737518878720"
+var CategoryID = "398083936745553920"
+
+var CurrentGame Game
+
+//Slices
+
+var AllTeams []Team
+var AllRoles []Role
+var Players []Player
+
+var Channels []string
+var TeamChannels []string
+var SpecialChannels []string
+var Connections []*websocket.Conn
+
+type Team struct {
+	Name        string
+	ChannelName string
+	HasChannel  bool
+}
+
+type Role struct {
+	Name        string `json:"name"`
+	Image       string `json:"card"`
+	ChannelName string `json:"-"`
+	Description string `json:"-"`
+	Team        Team   `json:"-"`
+}
+
+type RoleChannel struct {
+	Role Role
+	ID   string
+}
+
+type TeamChannel struct {
+	Team Team
+	ID   string
+}
+
+type Player struct {
+	Username   string `json:"username"`
+	ID         string `json:"id"`
+	AvatarURL  string `json:"picture"`
+	Infected   bool   `json:"-"`
+	ManualMute bool   `json:"-"`
+	Role       Role   `json:"role"`
+}
+
+type Game struct {
+	Started   bool
+	Finished  bool
+	SendComp  bool
+	Players   []Player
+	Roles     []Role
+	GuildID   string
+	Channels  []*discordgo.Channel
+	GameStats *discordgo.Channel
+	Votes     *discordgo.Channel
+	Deads     *discordgo.Channel
+}
